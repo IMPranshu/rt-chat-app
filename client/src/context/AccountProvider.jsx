33ebaf72ce9425d4e5a4505b59f2c2540947269a
Pranshu Kumar Agrawal <pranshukumar1999@gmail.com>
2023-10-25 12:37:34 +0530

    add socket.io-client in frontend

--- client/src/context/AccountProvider.jsx
+++ client/src/context/AccountProvider.jsx
@@ -1,10 +1,17 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useRef, useEffect } from "react";
 
+import { io } from "socket.io-client";
 export const AccountContext = createContext(null);
 
 const AccountProvider = ({ children }) => {
   const [account, setAccount] = useState();
   const [person, setPerson] = useState({});
+
+  const socket = useRef();
+
+  useEffect(() => {
+    socket.current = io(process.env.REACT_APP_SOCKET_URL);
+  }, []);
 
   return (
     <AccountContext.Provider
@@ -13,6 +20,7 @@ const AccountProvider = ({ children }) => {
         setAccount,
         person,
         setPerson,
+        socket,
       }}
     >
       {children}
