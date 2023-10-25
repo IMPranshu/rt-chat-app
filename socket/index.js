f67e3ee9e9792f6571adf8152266a1dfdd290c66
Pranshu Kumar Agrawal <pranshukumar1999@gmail.com>
2023-10-25 15:05:50 +0530

    fix for deployment

--- socket/index.js
+++ socket/index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: "https://rt-chat-app.vercel.app",
     methods: ["GET", "POST"],
   },
 });
